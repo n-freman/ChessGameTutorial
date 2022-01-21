18141053e52591ea30a4daa9a7a76125738451bb
Nazar <smiron1703@gmail.com>
2022-01-21 15:45:53 +0500

    added repr of game board

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Chess Making video tutorial completion
 
-I strated this tutorial on 21.01.2022.
-Hope to finish it in two weeks.
+I strated this tutorial on 21.01.2022.\
+Hope to finish it in two weeks.\
+======
+### About my own changes to the project
+I am also going to add few lines to resize images of pieces.\
+I saw that in tutorial author uses pictures of prepared size.
---
+++ chess_engine.py
@@ -0,0 +1,22 @@
+"""
+This class is responsible for storing all the information about the current state of a chess game. It will also be
+responsible for determining the valid moves at the current state. It will also keep a move log. 
+"""
+class GameState:
+
+    def __init__(self):
+        # The board is 8x8 2d list, each element of a list has 2 characters.
+        # The first character represents the color of the piece, 'b' or 'w'
+        # The second character represents the type of the piece, 'K', 'Q', 'R', 'B', 'N' or 'P'
+        # "--" represents an empty space with no piece.
+        self.board = [
+            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+            ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
+        ]
+
