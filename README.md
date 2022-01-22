58ecd2f4a032d217d68af124a74fef6a3646ca6f
Guga's PC <smiron1703@gmail.com>
2022-01-22 11:50:46 +0500

    created main function with main game loop

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Chess Making video tutorial completion\
-I strated this tutorial on 21.01.2022.\
+# Chess Making video tutorial completion
+I strated this tutorial on 21.01.2022.<br>
 Hope to finish it in two weeks.
 ======
 ### About my own changes to the project
-I am also going to add few lines to resize images of pieces.\
-I saw that in tutorial author uses pictures of prepared size.\
-Added some changes to the variable names, because author used not pythonic naming.\
+I am also going to add few lines to resize images of pieces.<br>
+I saw that in tutorial author uses pictures of prepared size.<br>
+Added some changes to the variable names, because author used not pythonic naming.
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import chess_engine
 
 WIDTH = HEIGHT = 650
 DIMENSION = 8
-SQ_SIZE = 81.25
+SQ_SIZE = HEIGHT // DIMENSION
 FPS = 15
 IMAGES = {}
 """
@@ -18,3 +18,27 @@ def load_images():
     for piece in pieces:
         IMAGES[piece] = pg.transform.scale(pg.image.load(f"Assets/{piece}.png"), (SQ_SIZE, SQ_SIZE))
     # Note: we can access an image by saying "IMAGES['wP']"
+
+
+"""
+This main driver for our code. This will handle user input and updating the graphics
+"""
+def main():
+    pg.init()
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    clock = pg.time.Clock()
+    screen.fill(pg.Color('white'))
+    gs = chess_engine.GameState()
+    load_images()
+    running = True
+
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+        clock.tick(FPS)
+        pg.diplay.flip()
+
+
+if __name__ == "__main__":
+    main()
